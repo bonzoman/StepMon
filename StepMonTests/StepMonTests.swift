8dc2dbc7637264d802e4e995fe62ebc6db0b64ff
sjo <bonzoman@gmail.com>
2026-01-24 21:25:21 +0900

    Initial Commit

---
+++ StepMonTests/StepMonTests.swift
@@ -0,0 +1,17 @@
+//
+//  StepMonTests.swift
+//  StepMonTests
+//
+//  Created by 오승준 on 1/24/26.
+//
+
+import Testing
+@testable import StepMon
+
+struct StepMonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
